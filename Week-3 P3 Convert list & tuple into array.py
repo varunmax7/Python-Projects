ba589d00f918ebd01e0f7d3cdaa901cf815c9d0f
VARUN <arunkumarramavath23@gmail.com>
2026-01-29 22:47:28 +0530

    Add files via upload
    new ones

---
+++ Week-3 P3 Convert list & tuple into array.py
@@ -0,0 +1,9 @@
+# Convert list and tuple into array
+
+import numpy as np
+my_list = [5, 6, 7, 39, 40, 1]
+print("List to array")
+print(np.asarray(my_list))
+my_tuple = (8, 14, 6, 9)
+print("List to tuple")
+print(np.asarray(my_tuple))
